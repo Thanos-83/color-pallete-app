8ee04e4aef6c70b0146b4464fc95edeeaa59d2e1
Thanos-83 <s_sbonias@hotmail.com>
2023-03-23 14:45:12 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,92 +1,93 @@
 import React, { Component } from 'react';
 import Palette from './Palette';
-import {Route, Switch} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 import seedColors from './seedColors';
-import {generatePalette} from './colorHelpers';
+import { generatePalette } from './colorHelpers';
 import PaletteList from './PaletteList';
 import SinlgeColorPalette from './SingleColorPalette';
 import NewPaletteForm from './NewPaletteForm';
 
-
-
-class App extends Component{
-
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
     this.state = {
-      palettes:savedPalettes || seedColors
-    }
+      palettes: savedPalettes || seedColors,
+    };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
     this.deletePalette = this.deletePalette.bind(this);
   }
-  findPalette(id){
-    const paletteID = this.state.palettes.find((palette)=>
-       palette.id===id
-    );
-    return paletteID
+  findPalette(id) {
+    const paletteID = this.state.palettes.find((palette) => palette.id === id);
+    return paletteID;
   }
-  savePalette(newPalette){
+  savePalette(newPalette) {
     // console.log(newPalette)
     this.setState({
-      palettes:[...this.state.palettes, newPalette]
-    })
+      palettes: [...this.state.palettes, newPalette],
+    });
   }
 
-  deletePalette(id){
+  deletePalette(id) {
     this.setState(
-      st=>({
-        palettes: st.palettes.filter(palette=> palette.id !== id)
+      (st) => ({
+        palettes: st.palettes.filter((palette) => palette.id !== id),
       }),
       this.syncLocalStorage
-    )
+    );
   }
 
-  syncLocalStorage=()=>{
+  syncLocalStorage = () => {
     window.localStorage.setItem(
       'palettes',
       JSON.stringify(this.state.palettes)
-    )
-  }
-  render(){  
+    );
+  };
+  render() {
     return (
       <Switch>
         <Route
           exact
           path='/palette/new'
-          render={(routeProps)=>(
-            <NewPaletteForm 
+          render={(routeProps) => (
+            <NewPaletteForm
               {...routeProps}
               palettes={this.state.palettes}
               savePalette={this.savePalette}
             />
           )}
         />
-        <Route 
-          exact 
-          path='/' 
-          render={(routeProps)=>(
-            <PaletteList 
-              palettes={this.state.palettes} 
-              {...routeProps} 
+        <Route
+          exact
+          path='/'
+          render={(routeProps) => (
+            <PaletteList
+              palettes={this.state.palettes}
+              {...routeProps}
               deletePalette={this.deletePalette}
             />
           )}
         />
-        <Route 
-          exact 
-          path='/palette/:id' 
-          render={(routeProps)=>(
-            <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>
+        <Route
+          exact
+          path='/palette/:id'
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
           )}
         />
         <Route
           exact
           path='/palette/:paletteId/:colorId'
-          render={(routeProps)=>(
-            <SinlgeColorPalette 
-              palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
+          render={(routeProps) => (
+            <SinlgeColorPalette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
               colorId={routeProps.match.params.colorId}
             />
           )}
@@ -96,7 +97,7 @@ class App extends Component{
       //   <Palette palette={generatePalette(seedColors[5])} />
       // </div>
     );
-    }
+  }
 }
 
 export default App;
